945ffc3fd660d2b0bb2ea68f15d1510785b92eae
Arindom <arindom793@gmail.com>
2026-09-04 21:14:55 +0600

    .

--- app/staff/page.tsx
+++ app/staff/page.tsx
@@ -6,7 +6,24 @@ export default function StaffPage() {
   return (
     <>
       <div>
-        
+        <aside>
+          <div>
+            <div>
+              Dwell
+              <span className="text-dwellix-500">ix</span>
+              <span className="ml-2 text-xs font-normal text-gray-400 uppercase tracking-widest">Staff</span>
+
+            </div>
+
+          </div>
+          <div>
+
+          </div>
+        </aside>
+        <div>
+
+        </div>
+
       </div>
     </>
   );
